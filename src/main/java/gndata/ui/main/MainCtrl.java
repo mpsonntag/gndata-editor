2af3e29059a4bf785e94b24230d2d1a47b959147
asobolev <sobolev.andrey@gmail.com>
2014-09-10 16:52:23 +0200

    moved fxtest application to the project

---
+++ src/main/java/gndata/ui/main/MainCtrl.java
@@ -0,0 +1,26 @@
+package gndata.ui.main;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainCtrl implements Initializable {
+
+    public BorderPane view;
+    @Inject
+    private MenuView menuView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            view.setTop(menuView.getScene());
+        } catch (IOException e) {
+            // TODO show error dialog and quit program
+            e.printStackTrace();
+        }
+    }
+}
