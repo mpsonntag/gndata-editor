71e211dc53c4e437b9d55aa8f36a53e67e8f9a60
asobolev <sobolev.andrey@gmail.com>
2014-09-11 13:39:01 +0200

    test template added

---
+++ src/test/java/gndata/ui/main/MainCtrlTest.java
@@ -0,0 +1,25 @@
+package gndata.ui.main;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainCtrlTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testInitialize() throws Exception {
+
+    }
+}
