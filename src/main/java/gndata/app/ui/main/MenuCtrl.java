7bf14d91ccab3d3c47acd220c474419a301973e9
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-09-19 17:10:55 +0200

    MenuCtrl: action for creating a new project

--- src/main/java/gndata/app/ui/main/MenuCtrl.java
+++ src/main/java/gndata/app/ui/main/MenuCtrl.java
@@ -1,28 +1,80 @@
 package gndata.app.ui.main;
 
 import gndata.app.state.AppState;
+import gndata.app.state.ProjectState;
+import gndata.app.ui.dia.ProjectConfigView;
+import gndata.lib.config.ProjectConfig;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.stage.DirectoryChooser;
 
 import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
 
+/**
+ * Controller for the main menu view.
+ */
 public class MenuCtrl {
 
-    @Inject
-    private AppState state;
+    @FXML private MenuBar menu;
 
-    public void openProject(ActionEvent event)
-    {
+    private final AppState appState;
+    private final ProjectState projectState;
+
+    @Inject
+    public MenuCtrl(AppState appState, ProjectState projectState) {
+        this.appState = appState;
+        this.projectState = projectState;
+    }
+
+    /**
+     * Open a new project or create one.
+     *
+     * @param event The event causing the call of the method.
+     */
+    public void createProject(ActionEvent event) {
+        DirectoryChooser dirChooser = new DirectoryChooser();
+        dirChooser.setTitle("Select the project directory");
+        dirChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        File selected = dirChooser.showDialog(menu.getScene().getWindow());
+
+        if (selected == null)
+            return;
+
+        try {
+            ProjectConfig config = ProjectConfig.load(selected.getAbsolutePath());
+            ProjectConfigView configDialog = new ProjectConfigView(config);
+            config = configDialog.showDialog(menu.getScene().getWindow());
+
+            if (config != null) {
+                config.store();
+                projectState.setConfig(config);
+            }
+        } catch (IOException e) {
+            // TODO nice exception dialog here
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Open a previously opened project.
+     *
+     * @param event The event causing the call of the method.
+     */
+    public void openProject(ActionEvent event) {
         System.out.println("openProject");
     }
 
-    public void createProject(ActionEvent event)
-    {
-        System.out.println("createProject");
-    }
-
-    public void exit(ActionEvent e)
-    {
-        System.out.println("Called exit");
-        state.setRunning(false);
+    /**
+     * Set the project state to not running.
+     *
+     * @param event The event causing the call of the method.
+     */
+    public void exit(ActionEvent event) {
+        appState.setRunning(false);
     }
 }
