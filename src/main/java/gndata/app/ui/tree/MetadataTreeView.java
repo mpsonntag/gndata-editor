15483ae3ea895d2fda86a9b0306973ce794f66c0
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-10-13 09:44:55 +0300

    MetadataTreeView: adopt constructor

--- src/main/java/gndata/app/ui/tree/MetadataTreeView.java
+++ src/main/java/gndata/app/ui/tree/MetadataTreeView.java
@@ -1,8 +1,16 @@
 package gndata.app.ui.tree;
 
+import com.google.inject.*;
 import gndata.app.ui.util.DIView;
 
 /**
  * Tree view for the project metadata.
  */
-public class MetadataTreeView  extends DIView {}
+public class MetadataTreeView extends DIView {
+
+    @Inject
+    public MetadataTreeView(Injector injector) {
+        super(injector);
+    }
+
+}
