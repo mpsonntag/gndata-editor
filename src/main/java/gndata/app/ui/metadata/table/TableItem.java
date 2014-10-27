f348c663e6abe75852cb5e038961373d9d39e84c
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-10-27 12:01:01 +0200

    Copyright header

--- src/main/java/gndata/app/ui/metadata/table/TableItem.java
+++ src/main/java/gndata/app/ui/metadata/table/TableItem.java
@@ -1,8 +1,15 @@
+// Copyright (c) 2014, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
 package gndata.app.ui.metadata.table;
 
 import com.hp.hpl.jena.datatypes.RDFDatatype;
 import com.hp.hpl.jena.rdf.model.*;
-
 
 /**
  * Class that implements rendering of table items with RDF literals.
--- src/main/java/gndata/app/ui/metadata/table/TableView.java
+++ src/main/java/gndata/app/ui/metadata/table/TableView.java
@@ -1,7 +1,14 @@
+// Copyright (c) 2014, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
 package gndata.app.ui.metadata.table;
 
-import com.google.inject.Inject;
-import com.google.inject.Injector;
+import com.google.inject.*;
 import gndata.app.ui.util.DIView;
 
 /**
