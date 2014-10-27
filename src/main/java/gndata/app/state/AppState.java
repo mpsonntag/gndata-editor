11bb4763371f4600a253c1b21eeea40beee7b32c
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-10-27 09:49:48 +0300

    AppState: format, copyright, and comments

--- src/main/java/gndata/app/state/AppState.java
+++ src/main/java/gndata/app/state/AppState.java
@@ -1,10 +1,23 @@
+// Copyright (c) 2014, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
 package gndata.app.state;
+
+import javafx.beans.property.*;
 
 import com.google.inject.Singleton;
 import gndata.lib.config.GlobalConfig;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleObjectProperty;
 
+/**
+ * Class representing different basic states of the application.
+ * The class can be used (along with guice) tho share state information between
+ * parts of the application.
+ */
 @Singleton
 public class AppState {
 
@@ -20,23 +33,23 @@ public class AppState {
         return running.get();
     }
 
-    public SimpleBooleanProperty runningProperty() {
-        return running;
-    }
-
     public void setRunning(boolean running) {
         this.running.set(running);
+    }
+
+    public SimpleBooleanProperty runningProperty() {
+        return running;
     }
 
     public GlobalConfig getConfig() {
         return config.get();
     }
 
-    public SimpleObjectProperty<GlobalConfig> configProperty() {
-        return config;
-    }
-
     public void setConfig(GlobalConfig config) {
         this.config.set(config);
+    }
+
+    public SimpleObjectProperty<GlobalConfig> configProperty() {
+        return config;
     }
 }
