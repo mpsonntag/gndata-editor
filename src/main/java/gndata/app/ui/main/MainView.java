780404cf7faacd5f8c16cdc3f5700e981a237331
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-10-27 10:25:52 +0200

    Copyright header

--- src/main/java/gndata/app/ui/main/MainView.java
+++ src/main/java/gndata/app/ui/main/MainView.java
@@ -1,9 +1,17 @@
+// Copyright (c) 2014, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
 package gndata.app.ui.main;
+
+import javax.inject.Inject;
 
 import com.google.inject.Injector;
 import gndata.app.ui.util.DIView;
-
-import javax.inject.Inject;
 
 /**
  * The main view of the application.
