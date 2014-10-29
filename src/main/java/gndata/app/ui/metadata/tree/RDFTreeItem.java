ad8600627a9dbafb90217a94d90c3646116d8ca9
asobolev <sobolev.andrey@gmail.com>
2014-10-29 11:05:20 +0100

    fixed the tree event bug as it's annoying

--- src/main/java/gndata/app/ui/metadata/tree/RDFTreeItem.java
+++ src/main/java/gndata/app/ui/metadata/tree/RDFTreeItem.java
@@ -43,7 +43,7 @@ public class RDFTreeItem extends TreeItem<RDFNode> {
 
     @Override
     public boolean isLeaf() {
-        return getChildren().size() == 0;
+        return buildChildren().size() == 0;
     }
 
     private boolean isOntologyRelated(Statement st) {
