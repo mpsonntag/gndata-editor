b1a2ef9450356445ef7440b55be997790c8c883f
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-12-08 18:02:32 +0100

    MetadataNav: view and controller for navbar

---
+++ src/main/java/gndata/app/ui/metadata/MetadataNavCtrl.java
@@ -0,0 +1,45 @@
+package gndata.app.ui.metadata;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.*;
+
+import com.google.inject.Inject;
+import gndata.app.state.*;
+import gndata.app.ui.util.BreadCrumbNav;
+import gndata.lib.srv.ResourceAdapter;
+
+/**
+ * Controller for the metadata navigation bar.
+ */
+public class MetadataNavCtrl implements Initializable {
+
+    @FXML
+    private BreadCrumbNav<ResourceAdapter> navBar;
+
+    private final ProjectState projectState;
+    private final MetadataNavState navState;
+
+    @Inject
+    public MetadataNavCtrl(ProjectState projectState, MetadataNavState navState) {
+        this.projectState = projectState;
+        this.navState = navState;
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        navBar.setItems(navState.getNavigationPath());
+
+        navState.selectedParentProperty().bind(navBar.getSelectionModel().selectedItemProperty());
+        navState.selectedParentProperty().addListener((p, o, n) -> {
+            navBar.getSelectionModel().select(n);
+        });
+
+        projectState.configProperty().addListener((p, o, n) -> {
+            if (navState.getFavoriteFolders().size() > 0) {
+                navState.setSelectedParent(navState.getFavoriteFolders().get(0));
+            }
+        });
+    }
+}
