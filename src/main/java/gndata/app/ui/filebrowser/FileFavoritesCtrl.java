e06562f7d878f383cdc2cb02a911bfa40f56e3b7
M. Sonntag <michael.p.sonntag@gmail.com>
2014-12-09 14:14:36 +0100

    FileBrowser: Favorites Mouse selection replaced with Listener

--- src/main/java/gndata/app/ui/filebrowser/FileFavoritesCtrl.java
+++ src/main/java/gndata/app/ui/filebrowser/FileFavoritesCtrl.java
@@ -5,12 +5,11 @@ import gndata.app.state.FileNavigationState;
 import gndata.app.state.ProjectState;
 import gndata.lib.srv.FileAdapter;
 import gndata.lib.srv.LocalFile;
-import javafx.event.EventHandler;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.nio.file.Path;
@@ -30,14 +29,9 @@ public class FileFavoritesCtrl implements Initializable {
 
     private final FileNavigationState navState;
 
-    private final ProjectState projectState;
-
     @Inject
     public FileFavoritesCtrl(ProjectState projectState, FileNavigationState navState){
 
-
-
-        this.projectState = projectState;
         this.navState = navState;
 
         projectState.configProperty().addListener((p, o, n) -> {
@@ -48,29 +42,17 @@ public class FileFavoritesCtrl implements Initializable {
             this.navState.getFavoriteFolders().add(new LocalFile(path));
             this.navState.getFavoriteFolders().add(new LocalFile(path.resolve(Paths.get("schemas"))));
         });
-
-
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         fileFavorites.setItems(navState.getFavoriteFolders());
-        navState.selectedFileProperty().bind(fileFavorites.getSelectionModel().selectedItemProperty());
-        fileFavorites.addEventHandler(MouseEvent.MOUSE_CLICKED, new SelectFileFavoriteHandler());
-    }
 
-    private class SelectFileFavoriteHandler implements EventHandler<MouseEvent> {
-
-        @Override
-        public void handle(MouseEvent event) {
-            FileAdapter fa = fileFavorites.getSelectionModel().getSelectedItem();
-            // TODO this check should not be required, only directories should be eligible as favorite items
-            if (! fa.isDirectory()) {
+        fileFavorites.getSelectionModel().selectedItemProperty().addListener((p, o, n) -> {
+            if (n == null)
                 return;
-            }
-            navState.setSelectedParent(fa);
-        }
+            navState.setSelectedParent(n);
+        });
     }
-
 }
