1f8604be48a2995e9048d326329793906f33ca0a
asobolev <sobolev.andrey@gmail.com>
2014-12-15 14:58:17 +0100

    implemented CheckListView in calendar

--- src/main/java/gndata/app/ui/calendar/favorites/CalendarFavoritesCtrl.java
+++ src/main/java/gndata/app/ui/calendar/favorites/CalendarFavoritesCtrl.java
@@ -16,6 +16,7 @@ import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.vocabulary.*;
 import gndata.app.state.*;
 import gndata.lib.srv.ResourceAdapter;
+import org.controlsfx.control.CheckListView;
 
 /**
  * Controller for the list of favorites to display.
@@ -23,7 +24,7 @@ import gndata.lib.srv.ResourceAdapter;
 public class CalendarFavoritesCtrl implements Initializable {
 
     @FXML
-    private ListView<SelectableResource> favoritesList;
+    private CheckListView<ResourceAdapter> favoritesList;
 
     private final ProjectState ps;
     private final CalendarState cs;
@@ -38,56 +39,25 @@ public class CalendarFavoritesCtrl implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Callback<ListView<SelectableResource>, ListCell<SelectableResource>> forListView =
-                CheckBoxListCell.forListView(SelectableResource::selectedProperty);
-        favoritesList.setCellFactory(forListView);
-
         updateFavorites();
     }
 
     private void updateFavorites() {
-        ObservableList<SelectableResource> lst = FXCollections.observableArrayList();
+        ObservableList<ResourceAdapter> lst = FXCollections.observableArrayList();
 
         if (ps.getMetadata() != null) {
             lst.addAll(ps.getMetadata().getAvailableTypes()
                     .stream()
-                    .map(r -> new SelectableResource(r, null))
+                    .map(r -> new ResourceAdapter(r, null))
                     .collect(Collectors.toList()));
         }
 
-        // setup listeners for all checkboxes
-        lst.forEach(sr -> sr.selectedProperty().addListener(
-                (p, o, n) -> cs.getSelectedTypes().setAll(
-                        favoritesList.getItems()
-                                .filtered(SelectableResource::getSelected)))
-        );
-
         favoritesList.setItems(lst);
-    }
+        favoritesList.getCheckModel().checkAll();
 
-    /**
-     * A helper class to make ResourceAdapter objects selectable in the list.
-     */
-    class SelectableResource extends ResourceAdapter {
-
-        private final SimpleBooleanProperty selected;
-
-        public SelectableResource(Resource resource, ResourceAdapter parent) {
-            super(resource, parent);
-
-            this.selected = new SimpleBooleanProperty(true);
-        }
-
-        public boolean getSelected() {
-            return selected.get();
-        }
-
-        public void setSelected(boolean selected) {
-            this.selected.set(selected);
-        }
-
-        public SimpleBooleanProperty selectedProperty() {
-            return selected;
-        }
+        favoritesList.getCheckModel().getCheckedItems().addListener(
+                (ListChangeListener.Change<? extends ResourceAdapter> l) ->
+                    cs.getSelectedTypes().setAll(favoritesList.getCheckModel().getCheckedItems())
+        );
     }
 }
