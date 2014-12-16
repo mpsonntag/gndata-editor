f60f4f4a078f8f99eae1191285112f1b327b722a
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-12-16 14:57:52 +0100

    Dashboard: dash renders fake project data

--- src/main/java/gndata/app/ui/dash/DashboardCtrl.java
+++ src/main/java/gndata/app/ui/dash/DashboardCtrl.java
@@ -1,31 +1,40 @@
 package gndata.app.ui.dash;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import javax.inject.Inject;
 import javafx.fxml.FXML;
 import javafx.scene.web.WebView;
 
 import gndata.app.html.PageCtrl;
+import gndata.app.state.ProjectState;
 
 /**
- * Created by stoewer on 16.12.14.
+ * Controller for the dashboard.
  */
 public class DashboardCtrl extends PageCtrl {
 
     @FXML
     private WebView webView;
 
+    private ProjectState projectState;
+
+    @Inject
+    public DashboardCtrl(ProjectState projectState) {
+        this.projectState = projectState;
+
+        this.projectState.configProperty().addListener((p, o, n) -> {
+            getPage().applyModel(new DashboardData());
+        });
+    }
+
     @Override
     public WebView getWebView() {
         return webView;
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        super.initialize(location, resources);
-
-        DashboardData model = new DashboardData("Cool name", "Cool description");
-
-        getPage().applyModel(model);
+    /**
+     * Just a demo for accessing the controller from the page.
+     */
+    public void goToMetadata() {
+        System.out.println("Go to metadata");
     }
 }
--- src/main/java/gndata/app/ui/dash/DashboardData.java
+++ src/main/java/gndata/app/ui/dash/DashboardData.java
@@ -1,16 +1,42 @@
 package gndata.app.ui.dash;
 
+import java.util.*;
+import static java.util.Collections.*;
+
 /**
- * Created by stoewer on 16.12.14.
+ * Dummy implementation for dashboard data.
  */
 public class DashboardData {
 
-    String name;
-    String description;
+    private String name;
+    private String description;
 
-    public DashboardData(String name, String dscription) {
-        this.name = name;
-        this.description = dscription;
+    private List<String> scientists;
+    private List<String> experiments;
+    private List<String> recentSessions;
+
+    private Integer fileCount;
+    private Integer metadataCount;
+
+    public DashboardData() {
+        name = "Fish Project One";
+
+        description =
+                "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed " +
+                "eiusmod tempor incidunt ut labore et dolore magna aliqua. ";
+        description += description += description;
+
+        scientists = new LinkedList<>();
+        addAll(scientists, "Jan Grewe", "Jan Bender", "Thomas Wachtler");
+
+        experiments = new LinkedList<>();
+        addAll(experiments, "Fish tracker", "Fried fish tracker");
+
+        recentSessions = new LinkedList<>();
+        addAll(recentSessions, "Session 2014-10-20", "Session 2014-09-06");
+
+        fileCount = 123;
+        metadataCount = 309234;
     }
 
     public String getName() {
@@ -19,5 +45,25 @@ public class DashboardData {
 
     public String getDescription() {
         return description;
+    }
+
+    public List<String> getScientists() {
+        return scientists;
+    }
+
+    public List<String> getExperiments() {
+        return experiments;
+    }
+
+    public List<String> getRecentSessions() {
+        return recentSessions;
+    }
+
+    public Integer getFileCount() {
+        return fileCount;
+    }
+
+    public Integer getMetadataCount() {
+        return metadataCount;
     }
 }
